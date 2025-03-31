201e0e78517a485563eff0904dedc33a97d6fd46
Muskan <muskan@Muskans-MacBook-Pro.local>
2025-03-31 01:19:40 -0400

    Added the templates for cart and checkout page and routed them. Found blocker2(checkout page)

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name = "Products"),
+    path('', views.store, name = "products"),
     path('cart/' , views.cart , name = "cart"), 
     path('checkout', views.checkout , name = "checkout"),
 ]
